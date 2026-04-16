648dc2e99e2f8dfbc4adae0cf6bddd0624e2a19c
Nurudee Adedeji <djsmacker01@gmail.com>
2026-04-16 06:22:16 +0100

    Update service worker revision for PWA enhancements
    - Updated the revision number for the index.html file in the service worker to reflect the latest changes.
    - Ensured proper cache management and synchronization for improved offline support.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-af8ff8c3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tr2fm4j7p98"
+    "revision": "0.mrt2jppio58"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
